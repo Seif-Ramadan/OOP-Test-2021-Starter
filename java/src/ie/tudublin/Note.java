aeecf1212dca4ecdd63032c0284f1aa13059fc22
Seif-Ramadan <seifseifr@gmail.com>
2021-04-21 19:07:56 +0100

    1

---
+++ java/src/ie/tudublin/Note.java
@@ -0,0 +1,12 @@
+public class Note {
+
+    private char note;
+    private int duration;
+
+    public Note(char note, int duration) {
+        note = new char;
+        duration = new int;
+        this.note = note;
+        this.duration = duration;
+    }
+}
